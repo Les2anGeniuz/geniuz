40a09c4606fe9daaf2813e88e63b31d6d026f0ce
K4ZED <kenzaathallah.wijaya@gmail.com>
2025-12-04 20:38:48 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ import fakultasRoutes from './routes/fakultas.js'
 import kelasRoutes from './routes/kelas.js'
 import meRoutes from './routes/me.js'
 import materiRoutes from './routes/materi.js'
+import tugasRoutes from './routes/tugas.js'
+import pengumpulanTugasRoutes from './routes/pengumpulanTugas.js'
+import progressRoutes from './routes/progress.js'
+import mentorRoutes from './routes/mentor.js'
 
 dotenv.config()
 
@@ -27,6 +31,10 @@ app.use('/api/fakultas', fakultasRoutes)
 app.use('/api/kelas', kelasRoutes)
 app.use('/api/me', meRoutes)
 app.use('/api/materi', materiRoutes)
+app.use('/api/tugas', tugasRoutes)
+app.use('/api/pengumpulan', pengumpulanTugasRoutes)
+app.use('/api/progress', progressRoutes)
+app.use('/api/mentor', mentorRoutes)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Backend ready on http://localhost:${process.env.PORT || 5000}`)

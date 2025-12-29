ce69ece7bb64581b0c56d0cd4f48036c0217010d
K4ZED <kenzaathallah.wijaya@gmail.com>
2025-12-30 00:36:37 +0700

    x

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        // ✅ backend VPS kamu
-        destination: "http://157.230.251.113/api/:path*",
-      },
-    ];
-  },
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "sesvblqrcbdmnkfvugtk.supabase.co",
-        port: "",
         pathname: "/storage/v1/object/public/**",
       },
       {
         protocol: "https",
         hostname: "img.youtube.com",
-        port: "",
         pathname: "/vi/**",
       },
       {
@@ -33,13 +21,6 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-
-      // ✅ (OPSIONAL) kalau image kamu ada yang dari backend VPS (misal /uploads/...)
-      {
-        protocol: "http",
-        hostname: "157.230.251.113",
         pathname: "/**",
       },
     ],

66f149459f29344d3ba8411d4a3b2148ccbdf36f
K4ZED <kenzaathallah.wijaya@gmail.com>
2025-12-29 23:48:04 +0700

    x

--- app/userPage/page.tsx
+++ app/userPage/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Navbar from "../components/navbar";
-import HeroSection from "../components/heroSection";
-import FeatureSection from "../components/featureSection";
-import Promo from "../components/promo";
-import Events from "../components/event";
-import CTA from "../components/cta";
-import Footer from "../components/footer"
+import Navbar from "../../components/navbar";
+import HeroSection from "../../components/heroSection";
+import FeatureSection from "../../components/featureSection";
+import Promo from "../../components/promo";
+import Events from "../../components/event";
+import CTA from "../../components/cta";
+import Footer from "../../components/footer";
 
 export default function Page() {
   return (
